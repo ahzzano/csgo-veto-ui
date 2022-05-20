1275e0ef223374721b32ad1b4b38a28ed0bc7c27
W. Santos <w.santos.gg@gmail.com>
2022-05-20 14:09:20 +0800

    cleaning up some code

--- backend/index.js
+++ backend/index.js
@@ -4,32 +4,38 @@ const fs = require('fs')
 const app = express()
 const config = JSON.parse(fs.readFileSync('./config.json'))
 
+// Global Constants
+const PORT = 8000
+const TEAM_1 = 1
+const TEAM_2 = 2
+
 let state = {
     available_maps: config.maps,
+    turn: TEAM_1,
     team1_picks: [],
     team2_picks: []
 }
 
-PORT = 8000
-TEAM_1 = 1
-TEAM_2 = 2
-
 function pickMap(team, map) {
-    map = state.available_maps.filter((v) => map == v.name)
-    console.log(map)
+    filtered_maps = state.available_maps.filter((v) => map == v.name)
+
+    if(filtered_maps.length <= 0) {
+        throw new Error(`Map: ${map} does not exist`)
+    }
+
+    map = filtered_maps[0]
 
     if(team == TEAM_1) {
-        return;
+        state.turn = TEAM_2
+        return
     }
     
     if(team == TEAM_2) {
-        return;
+        state.turn = TEAM_1
+        return
     }
     throw new Error('invalid team argument')
 }
-
-console.log(state)
-pickMap(TEAM_1, "Mirage")
 
 // App Calls
 
