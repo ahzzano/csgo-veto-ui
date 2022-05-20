cc17426b5edaf4bf761c16cc64731df48506218a
W. Santos <w.santos.gg@gmail.com>
2022-05-20 14:32:02 +0800

    added banMap functions and refactored some code

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const e = require('express')
 const express = require('express')
 const fs = require('fs')
 
@@ -9,14 +10,15 @@ const PORT = 8000
 const TEAM_1 = 1
 const TEAM_2 = 2
 
-let state = {
+let currentState = {
     available_maps: config.maps,
     turn: TEAM_1,
     team1_picks: [],
-    team2_picks: []
+    team2_picks: [],
+    decider: undefined 
 }
 
-function pickMap(team, map) {
+function getMap(state, map) {
     filtered_maps = state.available_maps.filter((v) => map == v.name)
 
     if(filtered_maps.length <= 0) {
@@ -24,17 +26,46 @@ function pickMap(team, map) {
     }
 
     map = filtered_maps[0]
+    return map
+}
 
-    if(team == TEAM_1) {
+function changeTurnState(state) {
+    if(state.turn == TEAM_1) {
         state.turn = TEAM_2
-        return
+    }
+    else {
+        state.turn = TEAM_1
+    }
+    return state
+}
+
+function pickMap(state, mapName) {
+    map = getMap(state, mapName)
+
+    if(state.turn == TEAM_1) {
+        state.team1_picks.push(map)
     }
     
-    if(team == TEAM_2) {
-        state.turn = TEAM_1
+    else if(state.turn == TEAM_2) {
+        state.team2_picks.push(map)
+    }
+    else {
+        throw new Error('invalid team argument')
         return
     }
-    throw new Error('invalid team argument')
+    state.available_maps = state.available_maps.filter((v) => v != map)
+    state = changeTurnState(state)
+    return state
+}
+
+function banMap(state, mapName) {
+    map = getMap(state, mapName)
+
+    state.available_maps = state.available_maps.filter((v) => v != map)
+
+    changeTurnState(state)
+
+    return state
 }
 
 // App Calls
@@ -47,8 +78,12 @@ app.get('/get_config', (req, res) => {
     res.json(config)
 })
 
+app.get('/pick', (req, res) => {
+    res.redirect('/state')
+})
+
 app.get('/state', (req, res) => {
-    res.json(state)
+    res.json(currentState)
 })
 
 app.listen(PORT, () => {
