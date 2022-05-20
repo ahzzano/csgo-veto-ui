a3944eabcc5d6278203a264c95c84a566addde1a
W. Santos <w.santos.gg@gmail.com>
2022-05-20 14:05:45 +0800

    added a pickMap function

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,34 @@ const fs = require('fs')
 const app = express()
 const config = JSON.parse(fs.readFileSync('./config.json'))
 
-console.log(config)
+let state = {
+    available_maps: config.maps,
+    team1_picks: [],
+    team2_picks: []
+}
 
 PORT = 8000
+TEAM_1 = 1
+TEAM_2 = 2
 
-/*
-    THE API SHOULD RETURN
-    state: {
-        available_maps: []
-        team1_picks: []
-        team2_picks: []
-    }
+function pickMap(team, map) {
+    map = state.available_maps.filter((v) => map == v.name)
+    console.log(map)
 
-    event: {
-        type: "map_pick" | "side_pick" |  "map_ban"
-        team: A | B
+    if(team == TEAM_1) {
+        return;
     }
-*/
+    
+    if(team == TEAM_2) {
+        return;
+    }
+    throw new Error('invalid team argument')
+}
+
+console.log(state)
+pickMap(TEAM_1, "Mirage")
+
+// App Calls
 
 app.get('/', (req, res) => {
     res.json({hello: 'world'})
@@ -28,6 +39,10 @@ app.get('/', (req, res) => {
 
 app.get('/get_config', (req, res) => {
     res.json(config)
+})
+
+app.get('/state', (req, res) => {
+    res.json(state)
 })
 
 app.listen(PORT, () => {
